5e98ddd2bc3d6cdb27813fa564705464f0e72e69
Alexander <sashok.tsibin@gmail.com>
2022-03-21 17:03:28 +0700

    lab3 first commit

---
+++ lab3/src/main/java/model/Figure.java
@@ -0,0 +1,18 @@
+package main.java.model;
+
+import java.util.List;
+
+public enum Figure {
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z;
+    private List<Coordinates> dot;
+    private Figure(int... coordinates){
+
+    }
+
+}
