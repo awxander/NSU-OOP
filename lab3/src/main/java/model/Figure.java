22fa8973d48e24a943ca6d29d3deee836b4dd8c0
Alexander <sashok.tsibin@gmail.com>
2022-03-23 18:14:40 +0700

    tetris: the beggining

--- lab3/src/main/java/model/Figure.java
+++ lab3/src/main/java/model/Figure.java
@@ -1,9 +1,10 @@
 package main.java.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public enum Figure {
-    I,
+    I(0, 0,     1, 0,       2, 0,      3, 0),
     J,
     L,
     O,
@@ -11,7 +12,18 @@ public enum Figure {
     T,
     Z;
     private List<Coordinates> dot;
-    private Figure(int... coordinates){
+
+    private Figure(int... coordinates) {
+        dot = new ArrayList<Coordinates>();
+        for (int j = 0; j < coordinates.length; j += 2) {
+            dot.add(new Coordinates(coordinates[j],coordinates[j+1]));
+        }
+    }
+
+    Figure turnLeft(){
+
+    }
+    Figure turnRight(){
 
     }
 
