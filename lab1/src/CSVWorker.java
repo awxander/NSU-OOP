98e5fe4dae3b20fcf0c94660cc725075745bc7d5
Alexander <sashok.tsibin@gmail.com>
2022-03-23 18:28:08 +0700

    added try with resourses

--- lab1/src/CSVWorker.java
+++ lab1/src/CSVWorker.java
@@ -6,22 +6,13 @@ import java.io.IOException;
 
 public class CSVWorker {
     public static void main(String[] args) {
-        BufferedReader reader = null;
-        BufferedWriter writer = null;
-        try {
-            reader = new BufferedReader(new FileReader("input.txt"));
-            writer = new BufferedWriter(new FileWriter("output.csv"));
+        try(BufferedReader reader  = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.csv")))
+        {
             WordClass arr = new WordClass();
             arr.printDataInCSV(reader, writer);
         } catch (IOException ex) {
             System.err.println("Error while reading file:" + ex.getLocalizedMessage());
-        } finally {
-            try {
-                if (reader != null) reader.close();
-                if (writer != null) writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
